d60f4b5961765f44f60fca9b496f50bf0c799a8d
Kamilla-goddes <kamillakamisya@gmail.com>
2026-01-17 20:12:58 +0200

    виправила

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 let drink = "сік";
 switch (drink) {
-    case "Кава": console.log("каву"); 
-    case "Чай": console.log("чай"); 
-    case "Сік": console.log("сік");
+    case "Кава":
+        console.log("каву");
+        break;
+    case "Чай":
+        console.log("чай");
+        break;
+    case "Сік":
+        console.log("сік");
+        break;
+    default:
+        console.log("Невідомо");
 }
 
 let day = "Субота";
@@ -11,42 +19,75 @@ switch (day) {
     case "Вівторок":
     case "Середа":
     case "Четвер":
-    case "П’ятниця": console.log("Робочий день");
+    case "П’ятниця":
+        console.log("Робочий день");
+        break;
     case "Субота":
-    case "Неділя": console.log("Вихідний день");
+    case "Неділя":
+        console.log("Вихідний день");
+        break;
+    default:
+        console.log("Невідомо");
 }
 
 let month = 4;
 switch (month) {
     case 12:
     case 1:
-    case 2: console.log("Зима");
+    case 2:
+        console.log("Зима");
+        break;
     case 3:
     case 4:
-    case 5: console.log("Весна");
+    case 5:
+        console.log("Весна");
+        break;
     case 6:
     case 7:
-    case 8: console.log("Літо");
+    case 8:
+        console.log("Літо");
+        break;
     case 9:
     case 10:
-    case 11: console.log("Осінь");
+    case 11:
+        console.log("Осінь");
+        break;
+    default:
+        console.log("Невідомо");
 }
 
 let color = "жовтий";
 switch (color) {
-    case "червоний": console.log("стоп");
-    case "зелений": console.log("йти");
-    case "жовтий": console.log("чекати");
+    case "червоний":
+        console.log("стоп");
+        break;
+    case "зелений":
+        console.log("йти");
+        break;
+    case "жовтий":
+        console.log("чекати");
+        break;
+    default:
+        console.log("Невідомо");
 }
 
 let number1 = 10;
 let number2 = 0;
 let operator = "/";
 switch (operator) {
-    case "+": console.log(num1ber + number2);
-    case "-": console.log(number1 - number2);
-    case "*": console.log(number1 * number2);
-    case "/": 
+    case "+":
+        console.log(number1 + number2);
+        break;
+    case "-":
+        console.log(number1 - number2);
+        break;
+    case "*":
+        console.log(number1 * number2);
+        break;
+    case "/":
         if (number2 === 0) console.log("Помилка: ділення на нуль");
         else console.log(number1 / number2);
+        break;
+    default:
+        console.log("Невідомо");
 }
